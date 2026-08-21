57561a511684c8d9e1aaf809851a89a9af504791
Carlos <alfredone28@gmail.com>
2026-08-20 22:11:19 -0400

    feat: clase 20-08-26

--- citas/admin.py
+++ citas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import cliente, servicio
 
 # Register your models here.
+@admin.register(servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'duracion_min')
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+@admin.register(cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono')
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
--- citas/models.py
+++ citas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class cliente (models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+class servicio (models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    duracion_min = models.IntegerField(default=30)
+
+    def __str__(self):
+        return self.nombre
